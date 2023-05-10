d48e649fbe979a7b0c68ef2786136ae13d7649b4
999SH <94057132+999SH@users.noreply.github.com>
2023-05-10 15:54:09 +0200

    Day 5 done

--- day5/day5.js
+++ day5/day5.js
@@ -1,12 +1,42 @@
-/*
-                [V]     [C]     [M]
-[V]     [J]     [N]     [H]     [V]
-[R] [F] [N]     [W]     [Z]     [N]
-[H] [R] [D]     [Q] [M] [L]     [B]
-[B] [C] [H] [V] [R] [C] [G]     [R]
-[G] [G] [F] [S] [D] [H] [B] [R] [S]
-[D] [N] [S] [D] [H] [G] [J] [J] [G]
-[W] [J] [L] [J] [S] [P] [F] [S] [L]
- 1   2   3   4   5   6   7   8   9
+const fs = require('fs');
+const fileStream = fs.createReadStream('source.txt');
+const readline = require('readline');
 
- */
+const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity
+});
+
+const stacks = [
+    ['W','D','G','B','H','R','V'],
+    ['J','N','G','C','R','F'],
+    ['L','S','F','H','D','N','J'],
+    ['J','D','S','V'],
+    ['S','H','D','R','Q','W','N','V'],
+    ['P','G','H','C','M'],
+    ['F','J','B','G','L','Z','H','C'],
+    ['S','J','R'],
+    ['L','G','S','R', 'B', 'N', 'V', 'M']
+];
+
+let buffer = []
+rl.on('line', (line) => {
+    const [move, x, from,y,to,z] = line.split(' ');
+    console.log(x,y,z)
+    let i = 0
+    while(i < x){
+        buffer[i++] = stacks[y-1].pop()
+    }
+    while(i > 0){
+        stacks[z-1].push(buffer[--i])
+    }
+    buffer = []
+    i = 0
+});
+rl.on('close', () => {
+    let i = 0
+    while(i < 9){
+        console.log(stacks[i].pop())
+        i++
+    }
+});
